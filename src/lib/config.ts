9f61cfac1cd219b691f8008705df2c2e5db1e298
Lyresh Ann Bernardino <christianboluntate5@gmail.com>
2026-05-01 14:28:02 +0800

    fix vite env types

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,4 @@
+/// <reference types="vite/client" />
+
 export const NODE_API   = import.meta.env.VITE_NODE_API   ?? "http://localhost:5000";
 export const PYTHON_API = import.meta.env.VITE_PYTHON_API ?? "http://localhost:8000";
